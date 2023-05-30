c0664aa2355b15eb54a2f62034e57402fd1162a6
Varshith-p <varshithpuligadda@gmail.com>
2023-05-30 23:37:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <section className='flex flex-col justify-center items-center h-screen'>
+      <h1 className='font-medium text-3xl'>Atoms Group</h1>
+      <p className='text-gray-600 text-xl'>Bear with us, coming soon!!!</p>
+    </section>
+  )
+}
+
+export default App
