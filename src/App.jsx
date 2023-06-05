6180427d3080f933139c0c69012c127e5991d9ee
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-05 23:42:53 +0530

    basic installations done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
-
 function App() {
   return (
-    <section className='flex flex-col justify-center items-center h-screen'>
-      <h1 className='font-medium text-3xl'>Atoms Group</h1>
-      <p className='text-gray-600 text-xl'>Bear with us, coming soon!!!</p>
+    <section className="flex flex-col bg-white justify-center items-center h-screen">
+      <h1 className="font-medium text-primary text-3xl">Atoms Group</h1>
+      <p className="text-primary text-xl">Bear with us, coming soon!!!</p>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
